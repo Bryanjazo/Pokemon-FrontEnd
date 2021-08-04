9603dca21e021f9b822479a3cb8ea3e0207d62b9
Connor Holland <supersoccergoalie123@gmail.com>
2021-08-04 12:37:09 -0600

    Added grid to login page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@
 import './App.css';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import Loading from './components/loading';
+import { LoginPage } from './components/Login Component/logInPage';
 
 function App() {
   return (
     <Router>
-      <Route exact path='/' component={Loading}/>
+      <Route exact path='/' component={LoginPage}/>
     </Router>
   );
 }
---
+++ src/components/Login Component/logInPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './login_page.css'
+
+export const LoginPage = () => {
+    return (
+        <div className='login-grid'>
+        <section className='login-page'>
+            Hello
+        </section>
+        </div>
+    )
+}
