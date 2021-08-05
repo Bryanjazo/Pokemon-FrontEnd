bb717f767696304fac1d3e34ff5f3ad29fea8aa6
Bryan Jazo <bryanjazo88@gmail.com>
2021-08-05 10:01:35 -0700

    finished sessions controller working on users auth

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import Loading from './components/loading';
 import { LoginPage } from './components/Login Component/logInPage';
+import Login from './Oauth/SignIn.js'
 
 function App() {
   return (
     <Router>
       <Route exact path='/' component={LoginPage}/>
+        <Route exact path='/Login' component={Login}/>
     </Router>
   );
 }
