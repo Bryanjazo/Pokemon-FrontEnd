09651ea6161a63cc2ebebcb0c051a5b0e895131d
Connor Holland <supersoccergoalie123@gmail.com>
2021-08-05 11:04:01 -0600

    styled login page

--- src/components/Login Component/logInPage.js
+++ src/components/Login Component/logInPage.js
@@ -5,7 +5,17 @@ export const LoginPage = () => {
     return (
         <div className='login-grid'>
         <section className='login-page'>
-            Hello
+            <h2>Title</h2>
+            <form>
+                <label>Email:</label>
+                <input type='Text'/>
+                <label>Password:</label>
+                <input type='Password'/>
+                <div className='form-buttons'>
+                    <input className='submit' type='Submit' value='Login'/>
+                    <input className='submit' type='Button' value='Forgot password'/>
+                </div>
+            </form>
         </section>
         </div>
     )
