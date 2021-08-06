e517957a039c314133fe66809669d96ea4daaaa9
Connor Holland <supersoccergoalie123@gmail.com>
2021-08-06 12:57:12 -0600

    added auth reducer

---
+++ src/Reducer/authReducer.js
@@ -0,0 +1,7 @@
+let initialState = {}
+
+const authReducer = (state = initialState, action) => {
+    switch (action.type) {
+        default: return state
+    }
+}
--- src/Reducer/root_reducer.js
+++
@@ -1,9 +0,0 @@
-let stateInit = {
-}
-
-export default function rootReducer(state = stateInit, action) {
-    let clone = JSON.parse(JSON.stringify(state))
-    switch(action.type) {
-        default: return clone
-    }
-}
