b0e45ceec3b66df985c31b31cd4eb7ec78201fa3
Connor Holland <supersoccergoalie123@gmail.com>
2021-08-06 12:20:47 -0600

    Got switching between login and signing up working

--- src/components/Login Component/logInPage.js
+++ src/components/Login Component/logInPage.js
@@ -5,23 +5,36 @@ import './login_page.css'
 export const LoginPage = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [passwordTwo, setPasswordTwo] = useState('')
+    const [signUp, setSignUp] = useState(false)
 
     function handleSubmit(e) {
         e.preventDefualt()
     }
-    
+
+    function passwordReEntry() {
+        return (
+            <>
+                <label>Re-enter password:</label>
+                <input type="Password" onChange={e => setPasswordTwo(e.target.value)}/>
+            </>
+        )
+    }
+
     return (
         <div className='login-grid'>
         <section className='login-page'>
             <h2>Title</h2>
             <form onSubmit={e => handleSubmit(e)}>
                 <label>Email:</label>
-                <input type='Text'/>
+                <input type='Text' onChange={e => setEmail(e.target.value)}/>
                 <label>Password:</label>
-                <input type='Password'/>
+                <input type='Password' onChange={e => setPassword(e.target.value)}/>
+                {signUp && passwordReEntry()}
                 <div className='form-buttons'>
-                    <input className='submit' type='Submit' value='Login'/>
-                    <input className='submit' type='Button' value='Forgot password'/>
+                    <input className='submit' type='Submit' value={signUp ? 'Confirm' : 'Login'} readOnly/>
+                    <input className='submit' type='Button' value={signUp ? 'Back to login' : 'Sign Up'} onClick={ e => setSignUp(signUp ? false : true)} readOnly/>
+                    <input className='submit' type='Button' value='Forgot password' readOnly/>
                 </div>
             </form>
         </section>
