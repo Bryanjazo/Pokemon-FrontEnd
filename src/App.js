14f8eebb77efe91312a6ec23a30266b94d9f8cde
--add <andrewksmoker@gmail.com>
2021-08-06 14:54:21 -0400

    Updated Pokemon Reducer

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@
 import './App.css';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import Loading from './components/loading';
-import { LoginPage } from './components/Login Component/logInPage';
+import { LoginPage } from './components/loginComponent/logInPage';
 import Login from './Oauth/SignIn.js'
+import PokemonsContainer from './components/Pokemon/PokemonsContainer';
+
 
 function App() {
   return (
     <Router>
       <Route exact path='/' component={LoginPage}/>
-        <Route exact path='/Login' component={Login}/>
+        <Route exact path='/login' component={Login}/>
+        <Route exact path='/pokemon' component={PokemonsContainer}/>
     </Router>
   );
 }
--- src/components/Pokemon/PokemonsContainer.js
+++ src/components/Pokemon/PokemonsContainer.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const PokemonsContainer = () => {
+    useEffect
     return (
         <div>
             
