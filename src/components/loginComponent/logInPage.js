5249f18ecc99363786f15e13911d85aed30016c0
Connor Holland <supersoccergoalie123@gmail.com>
2021-08-06 15:33:13 -0600

    Fixed some styling on login page

--- src/components/loginComponent/logInPage.js
+++ src/components/loginComponent/logInPage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 import './login_page.css'
+import { signIn } from '../../actions/authentication';
 
 export const LoginPage = () => {
     const [email, setEmail] = useState('')
@@ -10,13 +11,19 @@ export const LoginPage = () => {
 
     function handleSubmit(e) {
         e.preventDefualt()
+
+        if(signUp) {
+            // Perform sign up functionality
+        } else {
+            signIn(email, password)
+        }
     }
 
     function passwordReEntry() {
         return (
             <>
                 <label>Re-enter password:</label>
-                <input type="Password" onChange={e => setPasswordTwo(e.target.value)}/>
+                <input className='main' type="Password" onChange={e => setPasswordTwo(e.target.value)}/>
             </>
         )
     }
@@ -24,18 +31,18 @@ export const LoginPage = () => {
     return (
         <div className='login-grid'>
         <section className='login-page'>
-            <h2>Title</h2>
+            <h1>Pokemon Battle Royal</h1>
             <form onSubmit={e => handleSubmit(e)}>
                 <label>Email:</label>
-                <input type='Text' onChange={e => setEmail(e.target.value)}/>
+                <input className='main' type='Text' onChange={e => setEmail(e.target.value)}/>
                 <label>Password:</label>
-                <input type='Password' onChange={e => setPassword(e.target.value)}/>
+                <input className='main' type='Password' onChange={e => setPassword(e.target.value)}/>
                 {signUp && passwordReEntry()}
                 <div className='form-buttons'>
                     <input className='submit' type='Submit' value={signUp ? 'Confirm' : 'Login'} readOnly/>
                     <input className='submit' type='Button' value={signUp ? 'Back to login' : 'Sign Up'} onClick={ e => setSignUp(signUp ? false : true)} readOnly/>
-                    <input className='submit' type='Button' value='Forgot password' readOnly/>
                 </div>
+                <input className='main' type='Button' value='Forgot password' readOnly/>
             </form>
         </section>
         </div>
