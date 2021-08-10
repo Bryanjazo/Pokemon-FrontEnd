bdd6164c9d2de257c69a4a96476b3fbd0d7acb45
--add <andrewksmoker@gmail.com>
2021-08-10 16:32:11 -0400

    slight updat

--- src/components/battle/BattleSelectContainer.js
+++ src/components/battle/BattleSelectContainer.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Pokemon from '../pokemon/Pokemon';
 
 const BattleSelectContainer = () => {
     const dispatch = useDispatch()
-    const userPokemon = useSelector(state => state.pokemonReducer.pokemon)
+    const [battlePokemon, setBattlePokemon] = useState([])
     
     return (
         <>
         <h1>Select Team</h1>
+        {battlePokemon.map(p => {
+            return (
+                <div></div>
+            )
+        })}
         <div className="battle-select-pokemon">
             <div>
                 <img src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png"}></img>
