d407fcd0a70cffc5381046582336b200a3aef5ef
--add <andrewksmoker@gmail.com>
2021-08-11 12:12:35 -0400

    Updating Battle Select page

--- src/components/battle/BattleSelectContainer.js
+++ src/components/battle/BattleSelectContainer.js
@@ -1,47 +1,58 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Pokemon from '../pokemon/Pokemon';
+import BattlePage from './BattlePage';
 
 const BattleSelectContainer = () => {
     const dispatch = useDispatch()
-    const [battlePokemon, setBattlePokemon] = useState([])
+    const [battlePokemon, setBattlePokemon] = useState([{}, {}, {}, {}, {}, {}])
+    const [count, setCount] = useState(0)
+    const [battleTime, setBattleTime] = useState(false)
+    const pokemon = useSelector(state => state.pokemonReducer.pokemon)
     
+    const handleBattleClick = (event) => {
+        event.preventDefault()
+        setBattleTime(true)
+    }
+    const handleClick = (event) => {
+        if (battlePokemon[5].id === undefined) {
+            event.target.style.display = "none"
+            let selected = pokemon.find(({id}) => id === parseInt(event.target.id))
+            battlePokemon[count] = selected
+            setCount(count + 1)
+        }
+    }
+    if (battleTime === true) {
+        return (<BattlePage userBattleTeam={battlePokemon}/>)
+    } else {
     return (
         <>
-        <h1>Select Team</h1>
+        <h1>Select Team</h1><br></br>
+        {battlePokemon[5].id !== undefined ? <div className="battle-button"><button onClick={handleBattleClick}>BATTLE!</button></div> : null}
+        <div className="battle-select-pokemon">
         {battlePokemon.map(p => {
+            if (p.front_image) {
             return (
                 <div>
+                    <img src={p.front_image} style={{maxHeight: "96px", maxWidth: "96px"}}></img>
                 </div>
             )
+            } else {
+                return (
+                    <div>
+                    <img src={"https://www.pngitem.com/pimgs/m/492-4923347_pokemon-question-mark-sprite-hd-png-download.png"} style={{maxWidth: "50px", maxHeight: "50px"}}></img>
+                    </div> 
+                )
+            }
         })}
-        <div className="battle-select-pokemon">
-            <div>
-                <img src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png"}></img>
-            </div>
-            <div>
-                <img src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png"}></img>
-            </div>
-            <div>
-                <img src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png"}></img>
-            </div>
-            <div>
-                <img src={"https://www.pngitem.com/pimgs/m/492-4923347_pokemon-question-mark-sprite-hd-png-download.png"} style={{maxHeight: "60px", maxWidth: "60px"}}></img>
-            </div>
-            <div>
-                <img src={"https://www.pngitem.com/pimgs/m/492-4923347_pokemon-question-mark-sprite-hd-png-download.png"} style={{maxHeight: "60px", maxWidth: "60px"}}></img>
-            </div>
-            <div>
-                <img src={"https://www.pngitem.com/pimgs/m/492-4923347_pokemon-question-mark-sprite-hd-png-download.png"} style={{maxHeight: "60px", maxWidth: "60px"}}></img>
-            </div>
         </div>
         <div className="pokedex-container">
 
     {
-        userPokemon.map(p => {
+        pokemon.map(p => {
         return (
            <ul className="user-pokemon">
-            <li><button>Select</button><br></br></li>
+            <li><button id={p.id} onClick={handleClick}>Select</button><br></br></li>
             <Pokemon
             pokemon = {p}
             key = {p.id}
@@ -53,6 +64,7 @@ const BattleSelectContainer = () => {
     </div>
         </>
     );
+}
 }
 
 export default BattleSelectContainer;
