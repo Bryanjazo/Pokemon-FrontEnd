d4c38533120645842358283656277ab331a7354e
--add <andrewksmoker@gmail.com>
2021-08-11 14:54:28 -0400

    updating battle page

--- src/components/battle/BattlePage.js
+++ src/components/battle/BattlePage.js
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import BattleMenu from './BattleMenu';
 import './battle.css'
 
 const BattlePage = (props) => {
+    const [selectedPokemon, setSelectedPokemon] = useState({})
+
+    const handleImageClick = (event) => {
+        event.preventDefault()
+        setSelectedPokemon(props.userBattleTeam[event.target.id])
+    }
     return (
-        
+        <>
         <div className="battle-page">
             <div className="battle-menu">
                 {<BattleMenu/>}
-                {props.userBattleTeam.map(p => {
-                return (
-                    <div className="user-battle-team">
-                        <img src={p.back_image}/>
-                    </div>
-                )
-            })}
             </div>  
+            <div className="user-battle-team">
+            <h3>My Team</h3>
+                {props.userBattleTeam.map((p, index) => {
+                    if (selectedPokemon.id === p.id) return (<p></p>)
+                    else return (
+                        <img id={index} src={p.front_image} onClick={handleImageClick}/>
+                    )
+                })}
+            </div>
         </div>
+        {selectedPokemon.id ? <div className="selected-pokemon"><img src={selectedPokemon.back_image}></img></div> : null}
+        </>
     );
 }
 
